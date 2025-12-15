e5699f0dbc77f3db53d1eb071c22cc2cc29b4e7a
joannahjatap <jatapjoannah24@gmail.com>
2025-12-15 15:34:17 +0800

    Update ProductRepository.java

--- Repository/ProductRepository.java
+++ Repository/ProductRepository.java
@@ -1,8 +1,9 @@
-package com.grefiel.lab_7.Entity.Repository;
+package com.JATAP.lab_7.Entity.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.alabado.lab7.entity.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
+
 
