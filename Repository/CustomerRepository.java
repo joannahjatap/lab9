a94731f4c6edbde5b4b73d0c8333d8e3362a3df3
joannahjatap <jatapjoannah24@gmail.com>
2025-12-15 14:52:51 +0800

    Add files via upload

---
+++ Repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.JATAP.lab_7.Entity.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.alabado.lab7.entity.Customer;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+}
+
